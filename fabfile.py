cbff1025d98e630ea5993d76bfc7e6dbfc88075f
Simon Whitaker <sw@netcetera.org>
2017-12-26 13:59:23 +0000

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+from fabric.api import env, run
+
+PPPOE0='pppoe0'
+
+env.hosts = ['192.168.1.1']
+env.shell = '/bin/vbash --login -c'
+
+def run_edgeos(command):
+    run('/opt/vyatta/bin/vyatta-op-cmd-wrapper %s' % command)
+
+def disconnect(interface=PPPOE0):
+    run_edgeos('disconnect interface %s' % interface)
+
+def connect(interface=PPPOE0):
+    run_edgeos('connect interface %s' % interface)
+
+def reconnect(interface=PPPOE0):
+    disconnect()
+    connect()
