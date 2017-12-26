63f6100ed812e273344c51ab2d0e38bf4e372196
Simon Whitaker <sw@netcetera.org>
2017-12-26 14:02:45 +0000

    Make _run_edgeos a private function

--- fabfile.py
+++ fabfile.py
@@ -5,14 +5,14 @@ PPPOE0='pppoe0'
 env.hosts = ['192.168.1.1']
 env.shell = '/bin/vbash --login -c'
 
-def run_edgeos(command):
+def _run_edgeos(command):
     run('/opt/vyatta/bin/vyatta-op-cmd-wrapper %s' % command)
 
 def disconnect(interface=PPPOE0):
-    run_edgeos('disconnect interface %s' % interface)
+    _run_edgeos('disconnect interface %s' % interface)
 
 def connect(interface=PPPOE0):
-    run_edgeos('connect interface %s' % interface)
+    _run_edgeos('connect interface %s' % interface)
 
 def reconnect(interface=PPPOE0):
     disconnect()
